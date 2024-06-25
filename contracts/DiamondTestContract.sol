de7972430e8ee762c5b53c78b7652103a8ea8bbf
Roderik van der Veer <roderik@settlemint.com>
2024-06-25 14:39:11 +0200

    chore: move all files into solidity-diamond-bond

---
+++ contracts/DiamondTestContract.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "./libraries/LibDiamond.sol";
+import "./interfaces/IDiamondCut.sol";
+
+contract DiamondTestContract {
+    using LibDiamond for *;
+
+    function setContractOwner(address _newOwner) external {
+        LibDiamond.setContractOwner(_newOwner);
+    }
+
+    function contractOwner() external view returns (address) {
+        return LibDiamond.contractOwner();
+    }
+
+    function diamondCut(
+        IDiamondCut.FacetCut[] memory _diamondCut,
+        address _init,
+        bytes memory _calldata
+    ) external {
+        LibDiamond.diamondCut(_diamondCut, _init, _calldata);
+    }
+
+    function enforceIsContractOwner() external view {
+        LibDiamond.enforceIsContractOwner();
+    }
+}
