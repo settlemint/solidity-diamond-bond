7086a5b774c7b9ac68a0d0dc93127406812d905e
Roderik van der Veer <roderik@settlemint.com>
2024-08-15 12:28:40 +0200

    feat: better docs and prepare for npm publishing

--- contracts/libraries/BokkyPooBahsDateTimeLibrary.sol
+++ contracts/libraries/BokkyPooBahsDateTimeLibrary.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: FSL-1.1-MIT
 pragma solidity >=0.6.0 <0.9.0;
 
 // ----------------------------------------------------------------------------
